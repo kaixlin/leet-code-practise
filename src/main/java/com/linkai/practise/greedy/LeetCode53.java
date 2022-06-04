70a26bbece4ddd3697157a93a87093353c1cf524
linkai <kai.x.lin@foxmail.com>
2022-06-04 14:09:58 +0800

    feat:新增leet code53

--- src/main/java/com/linkai/practise/greedy/LeetCode53.java
+++ src/main/java/com/linkai/practise/greedy/LeetCode53.java
@@ -1,2 +1,66 @@
-package com.linkai.practise.greedy;public class LeetCode53 {
+package com.linkai.practise.greedy;
+
+/**
+ * 给你一个整数数组 nums ，请你找出一个具有最大和的连续子数组（子数组最少包含一个元素），返回其最大和。
+ *
+ * 子数组 是数组中的一个连续部分。
+ *
+ *  示例 1：
+ * 输入：nums = [-2,1,-3,4,-1,2,1,-5,4]
+ * 输出：6
+ * 解释：连续子数组 [4,-1,2,1] 的和最大，为 6 。
+ *
+ * 示例 2：
+ * 输入：nums = [1]
+ * 输出：1
+ *
+ * 示例 3：
+ * 输入：nums = [5,4,-1,7,8]
+ * 输出：23
+ */
+public class LeetCode53 {
+    public static void main(String[] args) {
+        example1();
+        example2();
+        example3();
+    }
+
+    public static void example1() {
+        int[] nums = {-2,1,-3,4,-1,2,1,-5,4};
+        int expected = 6;
+        int actual = new LeetCode53().maxSubArray(nums);
+        System.out.println("示例1，输出结果：" + (actual == expected ? "成功" : "失败"));
+    }
+
+    public static void example2() {
+        int[] nums = {1};
+        int expected = 1;
+        int actual = new LeetCode53().maxSubArray(nums);
+        System.out.println("示例2，输出结果：" + (actual == expected ? "成功" : "失败"));
+    }
+
+    public static void example3() {
+        int[] nums = {5,4,-1,7,8};
+        int expected = 23;
+        int actual = new LeetCode53().maxSubArray(nums);
+        System.out.println("示例3，输出结果：" + (actual == expected ? "成功" : "失败"));
+    }
+
+    public int maxSubArray(int[] nums) {
+        int sum = 0;
+        int maxSum = nums[0];
+        for (int i = 0; i < nums.length; i++) {
+            //如果上次累计的和为负数，并且当前值大于sum，则证明需要从当前值开始重新计数
+            if (sum < 0 && nums[i] >= sum) {
+                sum = nums[i];
+            } else {
+                //否则进行累加
+                sum += nums[i];
+            }
+            if (sum > maxSum) {
+                maxSum = sum;
+            }
+        }
+        return maxSum;
+    }
 }
